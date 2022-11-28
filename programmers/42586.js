27637ba19f219ad8765dc16113086e8c03198d13
lmimoh <110901252+lmimoh@users.noreply.github.com>
2022-11-28 23:53:52 +0900

    2022.11.28 programmers 42586

--- programmers/42586.js
+++ programmers/42586.js
@@ -1,24 +1,24 @@
 function solution(progresses, speeds) {
     const answer = [];
-    
-    while(progresses.length > 0) {
-        const front = progresses.shift();
-        console.log(front);
-        const day = (100 - front) / speeds.shift();
-        const leftDay = Number.isInteger(day) ? day : day + 1;
-        let count = 1;
+    let localTime = 0;
+    let counter = 1;
+    for(let i = 0; i < progresses.length; i++) {
+        const time = (100 - progresses[i]) / speeds[i];
         
-        for(let i = 0; i < progresses.length; i++) {
-            if(progresses[i] + (speeds[i] * leftDay) >= 100) {
-                count++;
-            }
-            else {
-                progresses = progresses.slice(count - 1);
-                break;
+        if(localTime === 0) {
+            localTime = time;
+            if(!Number.isInteger(localTime)) localTime = parseInt(localTime) + 1;
+        } else {
+            if(localTime >= time) {
+                counter++;
+            } else {
+                answer.push(counter);
+                counter = 1;
+                localTime = time;
             }
         }
         
-        answer.push(count);
-    } 
+        if(i === progresses.length - 1) answer.push(counter);
+    }
     return answer;
 }
